b53c2d9e188abc92383bc3a9a4596db939e9c846
Stargnite <sobayotoby@gmail.com>
2022-03-10 05:49:43 -0800

    C introduction

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int t;
+	char o;
+	long long b;
+	long int i;
+	float l;
+
+	printf("Size of int: %lu bytes(s)\n", (unsigned long)sizeof(t));
+	printf("size of char: %lu bytes(s)\n", (unsigned long)sizeof(o));
+	print("size of long long int: %lu bytes(s)\n", (unsigned long)sizeof(b));	print("size of long int: %lu bytes(s)\n", (unsigned long)sizeof(i));
+	print("size of float: %lu bytes(s)\n", (unsigned long)sizeof(l));
+	return (0);
+}
