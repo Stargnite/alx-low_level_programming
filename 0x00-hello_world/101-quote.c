465fb483e825876634627055ca19b52ea55f7611
Stargnite <sobayotoby@gmail.com>
2022-03-10 07:42:21 -0800

    C introduction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Kopar, 2015-10-19\n", 59);
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
