f9294afcf44de1269c824d1e3a6bb7ea05f508d0
Stargnite <sobayotoby@gmail.com>
2022-03-11 05:46:54 -0800

    if/else statements

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numb;
+
+	for (numb = '0'; numb <= '9'; numb++)
+		putchar(numb);
+	putchar('\n');
+
+	return (0);
+}
