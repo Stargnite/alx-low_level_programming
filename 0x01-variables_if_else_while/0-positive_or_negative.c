fe6ffc9cda4db69266fd6c759dd975593c206503
Stargnite <sobayotoby@gmail.com>
2022-03-11 03:32:09 -0800

    A lesson on variables, if/else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	int i;
+	int n;
 	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -22,4 +22,4 @@ int main(void)
 	else(n < 0)
 		printf("%d is negative\n", n);
 	return (0);
-
+}
