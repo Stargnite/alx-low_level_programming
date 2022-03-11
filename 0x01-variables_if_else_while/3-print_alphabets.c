5f6aead451e5a8502c4f5b8c85aef773a9ebef91
Stargnite <sobayotoby@gmail.com>
2022-03-11 03:49:48 -0800

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char low;
@@ -12,7 +15,7 @@ int main(void)
 	for(low = 'a'; low <='z'; low++)
 	       putchar(low);
 	for(low = 'A'; low <= 'Z'; low++)
-		putchar(low);
+	       putchar(low);
         putchar('\n');
        
 	return (0);
