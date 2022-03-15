05260a6ba02c8c952dc40ffa7c5d6be7e0994770
Stargnite <sobayotoby@gmail.com>
2022-03-15 08:16:15 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet - check description
- * Description: a function that prints the alphabet, in lowercase, and new line
- *
- * no return.
- */
-void print_alphabet(void)
+* print_alphabet_x10 - Check holberton
+* Description: function uses _putchar function to print
+* alphabet in lowercase 10 times
+* Return: Nothing.
+*/
+
+oid print_alphabet_x10(void)
 {
-	char ch;
-
-	ch = 'a';
-
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
-
-	_putchar('\n');
-}
+	char i;
+	int j;
