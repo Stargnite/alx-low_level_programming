d604fd0f43979ab018939ce7d34531c8c53f166d
Stargnite <sobayotoby@gmail.com>
2022-03-15 12:27:15 -0700

    function

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += 1;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
