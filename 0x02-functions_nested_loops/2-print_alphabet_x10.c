8efa855801dd3664939fd39ab45208123b9e66ec
Stargnite <sobayotoby@gmail.com>
2022-03-15 07:58:51 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check code.
+ * print_alphabet_x10 - check code.
  *
  * Return: Always 0.
  */
@@ -14,7 +14,7 @@ void print_alphabet_x10(void)
 	{
 		for (i = 'a'; i < 'z'; i++)
 			_putchar(i);
-		_putchar("\n");
+		_putchar('\n');
 	}
 }
 
