8a1b46fbebf8c02bf5c63cf4764d3f2172a59f1e
Stargnite <sobayotoby@gmail.com>
2022-03-17 05:03:00 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,11 @@
 
 /**
  * _isupper - check code
- * Description: a function that checks for uppercase character.
  * @c: an input character
  * Return: 0 otherwise.
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	char uppercase = 'A';
 	int isupper = 0;
@@ -20,5 +19,6 @@ int _isupper(int c);
 			break;
 		}
 	}
+
 	return (isupper);
 }
