f58c79686e7ad23b4b85edbd86cb4b796389dcb7
Stargnite <sobayotoby@gmail.com>
2022-03-17 06:46:34 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
- * more_numbers - a function that prints 10 times the numbers, from 0 to 14 
- * followed by a new line.
+ * more_numbers - prints 0 - 14.
  *
  * Return: void.
  */
-
 void more_numbers(void)
 {
 	int i, j;
