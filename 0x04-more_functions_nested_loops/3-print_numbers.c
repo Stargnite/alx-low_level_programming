198346b5c25e37f9b3b1b3b56e8ba7a1677f6009
Stargnite <sobayotoby@gmail.com>
2022-03-17 05:40:16 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,12 +5,13 @@
  * Return: Always 0.
  * Description: a function that prints the numbers, from 0 to 9, followed by a new line.
  */
-
 void print_numbers(void)
 {
-	int i = '0';
+	int i;
 
-	for (; i <= '9'; i++)
-		_putchar(i);
+	for (i = 0; i < '10'; i++)
+	{
+		_putchar('0' + i);
+	}
 	_putchar('\n');
 }
