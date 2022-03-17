85dfd9f29628e9a4fe51e9747d3ab6d96ab62668
Stargnite <sobayotoby@gmail.com>
2022-03-17 05:50:58 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  * print_numbers - check code
- * Return: Always 0.
- * Description: a function that prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: void.
  */
 void print_numbers(void)
 {
