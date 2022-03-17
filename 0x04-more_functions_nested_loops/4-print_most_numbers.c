7e236198e4dd45d1a37864d241ff9a2354d223be
Stargnite <sobayotoby@gmail.com>
2022-03-17 05:58:14 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,20 @@
 #include "main.h"
-
 /**
  * print_most_numbers - check code
- * Return: Always 0.
+ *
+ * Return: void.
  */
-
 void print_most_numbers(void)
 {
 	int i = '0';
 
 	for (; i <= 9; i++)
 	{
-		if (i != '2' || i != '4')
-			_putchar(i);
+		if ((i == 2) || (i == 4))
+		{
+			continue;
+		}
+			_putchar('0' + i);
 	}
 	_putchar('\n');
 }
