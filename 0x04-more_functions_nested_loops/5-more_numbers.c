c9d4a7df84cb21772fc19111e867776dd4275e80
Stargnite <sobayotoby@gmail.com>
2022-03-17 06:41:43 -0700

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,20 @@
 
 void more_numbers(void)
 {
-	int i, j = 0;
+	int i, j;
 
-	for (; j <= 10; j++)
+	i = 0;
+	while (i < 10)
 	{
-		for (; i <= 14; i++)
+		j = 0;
+		while (j < 15)
 		{
-			if (i < 9)
-				_putchar(i / 10 + '0');
-
-			_putchar(i % 10 + '0');
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			j++;
 		}
 		_putchar('\n');
+		i++;
 	}
-}	
+}
