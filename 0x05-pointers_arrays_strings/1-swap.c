bcebe4505808a982529149c9970d0a2f02cc735f
Stargnite <sobayotoby@gmail.com>
2022-03-21 15:12:15 +0100

    pointers, array and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - check code
+ * @a: an input integer pointer
+ * @b: an input interger pointer
+ * Return: 0.
+ */
+void swap_int(int *a, int *b)
+{
+    int aux;
+
+    aux = *a;
+    *a = *b;
+    *b = aux;
+}
